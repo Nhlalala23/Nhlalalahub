c354b8e53ebd18fdc2dd33b1ab14c829a2cdf7e0
Nhlalala23 <mthombeninhlalala41@gmail.com>
2023-12-13 09:00:23 +0200

    lala

--- bootstrap/project2/new.php
+++ bootstrap/project2/new.php
@@ -8,9 +8,40 @@
 <body>
     <h1>Welcome</h1>
    <?php
+   $x=20;
+   $y=&$x;
+   $x=15;
+   var_dumb($z=5);
    $name ="Nhlalala"; 
    echo "Hello hi ".$name;
-   echo"welcome to my php";
+   echo"<br><n></n>welcome to my php ";
+    echo "<br\n the the of x is $x";
+    echo"<br\n y is: $y";
+
+    function myfunction(){
+        $n;
+        global $y ,$z;
+        $n = $y + $z; 
+        echo"<br\n variable n is: $n";
+
+    }
+    //static
+    myfunction();
+    function theFunction()
+    {
+        static $x=0;
+        echo $x;
+        $x++;
+    }
+    echo"<br\n";
+    theFunction();
+    echo"<br\n";
+    theFunction();
+    echo"<br\n";
+    theFunction();
+   
+
+
    ?>
 </body>
 </html>
