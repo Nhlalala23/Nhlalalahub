77b9c9917763de7c8232d0d0a0a3757ef644b0cb
Nhlalala23 <mthombeninhlalala41@gmail.com>
2024-02-19 15:49:39 +0200

    15:49

--- myweb/mydata.php
+++ myweb/mydata.php
@@ -15,7 +15,7 @@ if($conn->connect_error)
 }
 
 // creating tables
-/*$sql1 ="CREATE TABLE member (
+$sql1 ="CREATE TABLE member (
     meb_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(30) NOT NULL,
     lastname VARCHAR(30) NOT NULL,
@@ -30,7 +30,7 @@ if($conn->connect_error)
     }
     else{
         echo"Not connected: " . $conn->error ."<br>";
-    }*/
+    }
     
 
    $sql2 ="CREATE TABLE Account(
@@ -44,10 +44,41 @@ if($conn->connect_error)
 
       if($conn->query($sql2)===TRUE)
       {
-          echo"Table created";
+          echo"Table created <br>";
       }
       else{
-          echo"Not created: " . $conn->error;
+          echo"Not created: " . $conn->error ."<br>";
       }
+
+      $sql3 =" CREATE TABLE contribution(
+        contr_id  INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        amount int (100),
+        contr_date date)
+      ";
+
+if($conn->query($sql3)===TRUE)
+      {
+          echo"Table created <br>";
+      }
+      else{
+          echo"Not created: " . $conn->error ."<br>";
+      }
+
+      $sql4 ="CREATE TABLE memb_contr(
+        contr_id INT,
+        meb_id ,
+        PRIMARY KEY (contr_id,meb_id)
+        FOREIGN KEY(contr_id) REFERENCES contribution( contr_id)
+        FOREIGN KEY(meb_id) REFERENCES member(meb_id)
+
+        )
+        ";
+if($conn->query($sql4)===TRUE)
+{
+    echo"Table created <br>";
+}
+else{
+    echo"Not created: " . $conn->error ."<br>";
+}
 
 ?>
