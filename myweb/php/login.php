1c8b8a3f43c43eb8fc013b21555c246c740572ae
Nhlalala23 <mthombeninhlalala41@gmail.com>
2024-02-26 14:38:24 +0200

    14:38

--- myweb/php/login.php
+++ myweb/php/login.php
@@ -16,14 +16,14 @@ if ($conn->connect_error) {
 echo"Connected";
 
 //handling log in submission
-if($_SERVER["REQUEST_METHODE"] =="POST"){
+if($_SERVER["REQUEST_METHOD"] =="POST"){
     $user_name =$_POST['Email'];
     $pwd =$_POST['pswd'];
 
 //fetching database to fetch user details on database
 
     $sql ="SELECT * FROM account 
-    WHERE 'Email'='$user_name";
+    WHERE Email='$user_name'";
     $results =$conn->query($sql);
 
 
@@ -32,8 +32,22 @@ if($_SERVER["REQUEST_METHODE"] =="POST"){
 
         if(password_verify($pwd,$row['pswd'])){
             //password_verify to verify if the password matches
+            $_SESSION['memb_id'] = $row['id'];
+            $_SESSION['email'] = $row['Email'];
+
+            //redirecting to log-in page
+
+            header("Location: dashboard.php");
+            exit();
             
+
         }
+        else{
+            echo"Invalid username or password";
+        }
+    }
+    else{
+        echo"User not found";
     }
 
 
