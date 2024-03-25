1983883dcede7db557b4ddec35a79682871dd6ea
Nhlalala23 <mthombeninhlalala41@gmail.com>
2024-03-25 11:37:06 +0200

    11:37

--- test.java
+++ test.java
@@ -1,7 +1,12 @@
+/// syntax
 public class test{
-    public static void main(String[] args) throws
-    Exception{
+    public static void main(String[] args){
         System.out.println("Hello, world");
+        System.out.println("well yeah i don't know what's happening");
+        System.out.print("hello"); // no new line
+        System.out.println("Hello, world");
+
+        System.out.println(23 + "yep");
     }
 }
 
