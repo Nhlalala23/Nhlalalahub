5e31951d717fbcc71e66a161e8c0f776137442ab
Nhlalala23 <mthombeninhlalala41@gmail.com>
2024-03-25 12:23:58 +0200

    12:23

--- test.java
+++ test.java
@@ -7,6 +7,21 @@ public class test{
         System.out.println("Hello, world");
 
         System.out.println(23 + "yep");
+
+        //java casting
+        /*-widening casting->automatically - converting smaller number type to larger type size
+        Narrowing casting->manually- coverting larger to smaller */
+        int mynum= 19;
+        double mydouble =mynum;
+
+        System.out.println(mynum);
+       System.out.println(mydouble);
+
+       //Strings
+       String greeting ="Hello";
+
+       //string length
+       System.out.println("length = " + greeting.length());
     }
 }
 
