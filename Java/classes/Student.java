e1840e9d4e07d3de828fd0de6aaab5074d15526d
Nhlalala23 <mthombeninhlalala41@gmail.com>
2024-03-26 14:55:30 +0200

    14:55

---
+++ Java/classes/Student.java
@@ -0,0 +1,24 @@
+package Java.classes;
+
+//fields
+public class Student {
+    String name;
+    String student_id;
+    String course;
+
+    // Contructor
+    public Student(String name, int student_id,String course)
+    {
+        this.name ="Nhlalala";
+        this.student_id ="21677546";
+        this.course ="Software development";
+
+    }
+
+    //Method
+
+    public void drive(){
+        System.out.println("for 4 years");
+    }
+    
+}
