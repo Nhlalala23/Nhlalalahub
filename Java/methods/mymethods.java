9b95c0b8aa23432ce518244474f12d30d56deeb2
Nhlalala23 <mthombeninhlalala41@gmail.com>
2024-03-26 11:54:23 +0200

    11:54

--- Java/methods/mymethods.java
+++ Java/methods/mymethods.java
@@ -1,5 +1,5 @@
 
-public class mymethods{
+/*public class mymethods{
     // Create a checkAge() method with an integer variable called age
     static void checkAge(int age){
         if(age < 18){
@@ -13,4 +13,55 @@ public class mymethods{
     public static void main(String[] args){
         checkAge(19);
     }
-}
+}*/
+
+
+//java scope
+//class level scope
+//These variables are accessible throughout the class, including all methods and constructors within the class.
+/*public class mymethods{
+    private int classLevelVariable;
+
+    //methods can  access class-level variable
+
+    public void method1(){
+        classLevelVariable = 24;
+    }
+
+   
+}*/
+
+/*public class mymethods {
+    public void method1(){
+        // method-level variable ->only accessable withing only withing a method
+
+        int methodlevelvariable =10;
+        System.out.println(methodlevelvariable);
+    }
+
+    
+}*/
+
+//block level Scope -> vaeiables are obly accessable  only within the block in which they are declared
+
+/*public class mymethods {
+
+    public void method1(){
+        int blocklevelVariable = 13;
+        System.out.println(blocklevelVariable);
+
+    }
+}*/
+
+//loop control variable scope
+// These variables are accessible only within the loop in which they are declared. 
+ 
+public class mymethods {
+    public static void main() {
+        for(int i = 0; i < 20; i++)
+        {
+            System.out.println(i + 1);
+        }
+        
+    }
+} 
