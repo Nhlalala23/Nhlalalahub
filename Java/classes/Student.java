da4f1145858d6ec681a7ecb2ade1001711ee7161
Nhlalala23 <mthombeninhlalala41@gmail.com>
2024-03-28 12:38:03 +0200

    12:37

--- Java/classes/Student.java
+++ Java/classes/Student.java
@@ -12,8 +12,8 @@ public class Student {
     {
         this.name ="Nhlalala";
         this.student_id ="21677546";
-        this.course ="Software development";
-
+        this.course ="Computer Science";
+        this.age =18;
     }
 
     //Getter Method
@@ -26,13 +26,18 @@ public class Student {
         return age;
     }
 
-    public String courseName(){
+    public String getcourseName(){
         return course;
+    }
+    public String getstudent_id()
+    {
+        return student_id;
     }
     //Method o display student information
 
     public void information(){
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
+        System.out.println("Studet number:" + student_id);
     }
 }
--- Java/classes/StudentDemo.java
+++ Java/classes/StudentDemo.java
@@ -1,17 +1,23 @@
 package Java.classes;
 
 public class StudentDemo {
+
+
     public static void main(String[] args){
-        //instances ogf a student
-        Student student1 =new Student("lala", 20);
-        Student student2 =new Student("sie", 20);
+        //instance for student
+        Student  student1 = new Student("Nhlalala" ,2134567,"Computer Science");
+        
+        //display student information
 
-        //Displaying student info
-        System.out.println("Student 1: ");
-        student1.displayInfo();
+        System.out.println("Student Details");
+        System.out.println("Name: " + student1.getName());
+        System.out.println("Student Number: " + student1.getstudent_id());
+        System.out.println("Course name: " + student1.getcourseName());
+        System.out.println("==================");
+        student1.information();
 
-        System.out.println("\nStudent 2: ");
-        student2.displayInfo();
 
     }
 }
+
+
