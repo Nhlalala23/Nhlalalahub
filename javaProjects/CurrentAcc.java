6ad09a6950ace01c253d57a9d66885a251579941
Nhlalala23 <mthombeninhlalala41@gmail.com>
2024-04-09 11:59:07 +0200

    11:58

---
+++ javaProjects/CurrentAcc.java
@@ -0,0 +1,10 @@
+package javaProjects;
+
+public class CurrentAcc extends Account{
+    public CurrentAccount (double balance){
+        super(balance);
+    }
+    @Override
+    double interest();
+    return 0;
+}
