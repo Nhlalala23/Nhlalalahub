63f126afa83237fa84d5bd3794f66ff99ce9473b
Nhlalala23 <mthombeninhlalala41@gmail.com>
2024-04-22 23:27:53 +0200

    13:00

---
+++ javaform/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const form=document.getElementById('Form');
+    form.addEventListener('submit',function(event){
+        event.preventDefault();
+
+        const name = document.getElementById('name').value ;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        console.log('Name:${name},Emal:${email}Message: ${message}');
+
+        alert('Form submitted successfully');
+    });
+});
