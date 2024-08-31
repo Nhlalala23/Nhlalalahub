128db316e54adcd78ac807b8806fb6c6fdf836e3
Nhlalala23 <mthombeninhlalala41@gmail.com>
2024-08-31 18:57:23 +0200

    today

--- portfolio/ContactMessRepository.java
+++
@@ -1,9 +0,0 @@
-package portfolio;
-import java.util.List;
-
-
-
-public interface ContactMessRepository extends JpaRepository<ContactMess,Long> {
-
-    List<ContactMessage> findByEmail(String email);
-}
